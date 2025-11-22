4c2262db0b3347ef68384c1eb389024e1b3e879d
LuisAR219 <luis.alzate3@udea.edu.co>
2025-11-22 13:55:47 -0500

    Creación métodos EntidadJuego

--- DefFinal/entidadjuego.h
+++ DefFinal/entidadjuego.h
@@ -20,6 +20,23 @@ public:
     virtual void aplicarFuerza(const QVector2D& fuerza) = 0;
     virtual bool colisionaCon(const EntidadJuego* otra) const = 0;
 
+    QVector2D getPosicion() const;
+    void setPosicion(const QVector2D& pos);
+
+    QVector2D getVelocidad() const;
+    void setVelocidad(const QVector2D& vel);
+
+    float getVida() const;
+    void setVida(float v);
+
+    TipoEntidad getTipoEntidad() const;
+    void setTipoEntidad(TipoEntidad tipo);
+
+protected:
+    QVector2D posicion;
+    QVector2D velocidad;
+    float vida;
+    TipoEntidad tipo;
 };
 
 #endif // ENTIDADJUEGO_H
